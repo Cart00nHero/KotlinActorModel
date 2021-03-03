9fd9c98fd169e27a364497f2f7c20751a29219dc
Cart00nHero8 <Cart00nHero8@gmail.com>
2021-03-04 01:15:28 +0800

    gitflow-feature-stash: use_BroadcastChannel

--- ActorModel/app/src/main/java/com/cartoonhero/source/actormodel/ActorSystem.kt
+++ ActorModel/app/src/main/java/com/cartoonhero/source/actormodel/ActorSystem.kt
@@ -15,8 +15,8 @@ interface Message
 class ActorSystem {
     private val systemScope: CoroutineScope =
         CoroutineScope(EmptyCoroutineContext + SupervisorJob())
-    private val channel: Channel<Message> = Channel(100)
-//    private val channel: BroadcastChannel<Message> = BroadcastChannel(100)
+//    private val channel: Channel<Message> = Channel(100)
+    private val channel: BroadcastChannel<Message> = BroadcastChannel(100)
 
     fun send(event: Message) {
         systemScope.launch {
@@ -24,7 +24,7 @@ class ActorSystem {
         }
     }
 
-    val messages: Flow<Message>
-        get() = flow { emitAll(channel.receiveAsFlow()) }
-//        get() = flow { emitAll(channel.openSubscription()) }
+    val mailbox: Flow<Message>
+//        get() = flow { emitAll(channel.receiveAsFlow()) }
+        get() = flow { emitAll(channel.openSubscription()) }
 }
