f4a6096e0c539c6d91be4a82369d8d2ea0acb62a
Cart00nHero8 <Cart00nHero8@gmail.com>
2021-03-03 17:57:19 +0800

    first commit

---
+++ ActorModel/app/src/main/java/com/cartoonhero/source/actormodel/Actor.kt
@@ -0,0 +1,42 @@
+package com.cartoonhero.source.actormodel
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.actor
+import kotlinx.coroutines.flow.collect
+
+@ObsoleteCoroutinesApi
+@ExperimentalCoroutinesApi
+abstract class Actor {
+
+    private val scope = CoroutineScope(Dispatchers.Default + Job())
+    private val stream = ActorStream()
+    private data class ActorMessage(
+        val send: () -> Unit
+    ): Message
+
+    init {
+        start()
+    }
+
+    private fun start() = scope.launch {
+        val actor = actor<Message>(scope.coroutineContext) {
+            for (msg in channel) {
+                act(msg)
+            }
+        }
+        stream.messages.collect(actor::send)
+    }
+    private fun sendMessage(message: Message) = stream.send(message)
+    private fun act(message: Message) {
+        when(message) {
+            is ActorMessage -> message.send()
+        }
+    }
+
+    fun send(sender: () -> Unit) {
+        sendMessage(ActorMessage(sender))
+    }
+    fun cancel() {
+        scope.cancel()
+    }
+}
